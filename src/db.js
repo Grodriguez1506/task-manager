d4c025fdf1545db5b1664d64ee6a109bd6801887
Grodriguez1506 <garj.1706@gmail.com>
2025-01-19 13:07:41 -0500

    project configured to deploy in vercel

--- src/db.js
+++ src/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1/authusers");
+    await mongoose.connect(
+      "mongodb+srv://garj1706:RBmqWoSQmMr5m9s5@cluster0.0ydoi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log("connected to DB");
   } catch (error) {
     console.log(error);
